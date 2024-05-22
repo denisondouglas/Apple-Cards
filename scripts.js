73c0bf1d63ff1474a904dd265a91234af7b0273d
denisonsouglas <deouglas22@gmail.com>
2024-05-22 09:18:00 -0300

    HTML corrigido, falta finalizar o JS

--- scripts.js
+++ scripts.js
@@ -1,25 +1,3 @@
-const red = document.getElementsById("red-watch");
-const blue = document.getElementsByClassName("troca-cor-azul");
-const black = document.getElementsByClassName("troca-cor-preto");
+const switchcolor = document.getElementById("switch-color");
 
-const changeImg = (ColorChanging = () => {
-  const redWatch = document.getElementsById("red-watch");
-  const blueWatch = document.getElementsByClassName("troca-cor-azul");
-  const blackWatch = document.getElementsByClassName("troca-cor-preto");
-
-  if (red.click === true) {
-    redWatch.src = "./assets/Apple Watch Vermelho.webp";
-  }
-
-  if (blue.click === true) {
-    blueWatch.src = "./assets/Apple Azul.webp";
-  }
-
-  if (black.click === true) {
-    blackWatch.src = "./assets/Apple Watch Preto.webp";
-  }
-});
-
-red.addEventListener("click");
-blue.addEventListener("click");
-black.addEventListener("click");
+console.log(switchcolor);
